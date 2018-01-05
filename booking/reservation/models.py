cce921aaaf715fd359bd9f462f2484becb269291
matthew goodman <mattsgood2@aol.com>
2018-01-05 14:11:12 +0000

    reservation models started

--- booking/reservation/models.py
+++ booking/reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=150)
+    mobile_number = models.CharField(max_length=15)
+    time = models.DateTimeField()
+
+    task_id = models.CharField(max_length=50, blank=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Reservation {0} - {1}'.format(self.pk, self.name)
+        
